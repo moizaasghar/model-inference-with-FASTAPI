90475e52d8c4864fdd1c767755b83c7640af7d9e
Moiz Asghar <moiz.asghar@afiniti.com>
2025-08-03 23:22:15 +0500

    refactor: remove unused import statement from app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
-import os
 from pathlib import Path
 import logging
 
